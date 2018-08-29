c5934b7c5e0d50def55829055843a5ca2442b30e
Spencer Turkel <spencer@palmbeachsoftware.com>
2018-08-28 23:55:47 -0400

    Free var parser

--- hw1.py
+++ hw1.py
@@ -1,6 +1,8 @@
+import re
 from enum import Enum, auto
-from typing import Union, Optional, Callable, TypeVar, NewType, Tuple
+from typing import Union, Optional, Callable, TypeVar, NewType, Tuple, NamedTuple
 
+# Defining the grammar in types
 Formula = Union['FreeVar', 'SList']
 FreeVar = NewType('FreeVar', str)
 SList = Union['BinCall', 'NotCall']
@@ -14,13 +16,39 @@ class BinOp(Enum):
     OR = auto()
 
 
+# Defining the type of Parsers
 T = TypeVar('T')
-Parser = Callable[[str], Optional[T]]
+ParseResult = NamedTuple('ParseResult', [('rest', str), ('value', T)])
+Parser = Callable[[str], Optional[ParseResult[T]]]
 
 
-def formula_parser(free_var_parser: Parser[object], sexp_parser: Parser[None]) -> Parser[None]:
-    def parse(s: str) -> Optional[str]:
-        pass
+def run_parser(p: Parser[T]) -> Callable[[str], Optional[str]]:
+    def run(s: str) -> Optional[str]:
+        result = p(s)
+        if result is None:
+            return None
+        return result.value
+    return run
+
+
+def formula_parser(free_var_parser: Parser[Formula],
+                   s_exp_parser: Parser[SList]) -> Parser[None]:
+    return or_parser(free_var_parser, s_exp_parser)
+
+
+U = TypeVar('U')
+
+
+def or_parser(first_parser: Parser[T], second_parser: Parser[U]) -> Parser[Union[T, U]]:
+    return lambda s: first_parser(s) or second_parser(s)
+
+
+def free_var_parser() -> Parser[FreeVar]:
+    regex = re.compile('[a-z0-9]+')
+
+    def parse(s: str) -> Optional[ParseResult[FreeVar]]:
+        pat = regex.match(s)
+        return ParseResult(s, pat.group()) if pat is not None else None
 
     return parse
 
